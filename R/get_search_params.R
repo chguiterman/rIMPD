c81fea79b993ea4c0a8363d54fe29277235ce5d9
Chris Guiterman <chguiterman@email.arizona.edu>
2021-07-21 14:50:05 -0400

    create get_search_params()

---
+++ R/get_search_params.R
@@ -0,0 +1,44 @@
+#' Extract IMPD search parameters for `investigators` and `species`
+#'
+#' This function pulls form the updated store of contributor names and species
+#' to help users specify their searches
+#'
+#' @param output Two data sets are available, "investigators" (the default) and "species"
+#'
+#' @importFrom httr GET content
+#' @importFrom jsonlite fromJSON
+#' @importFrom readr read_delim
+#'
+#' @export
+#'
+#' @examples
+#' # Retrieve the list of IMPD contributors
+#' get_search_params("investigators")
+#'
+#' # Retrieve a data frame of species codes
+#' get_search_params("species")
+
+get_search_params <- function(output = c("investigators", "species")) {
+  resp <- GET("https://www.ncdc.noaa.gov/paleo-search/study/params.json")
+
+  params <- fromJSON(content(resp, "text", encoding = "UTF-8"),
+                     simplifyDataFrame = TRUE)
+
+  investigators <- params[["investigators"]][["NOAA"]][["12"]]
+  species_list <- params[["species"]][["NOAA"]][["12"]]
+  species_df <- read_delim(species_list, delim = ":",
+                           col_names = c("species", "spp_code"))
+
+  output <- match.arg(output)
+  # switch(output,
+  #        investigators = "investigators",
+  #        species = "species")
+
+  if (output == "investigators") {
+    out <- investigators
+  }
+  if (output == "species") {
+    out <- species_df
+  }
+  out
+}
