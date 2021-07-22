4c65f7ffe84aff2becfaaf53117ecd85783f637f
Chris Guiterman <chguiterman@email.arizona.edu>
2021-07-22 15:49:01 -0400

    run functions inside of tests to (hopefully) please R CMD Check

--- tests/testthat/test-get_search_params.R
+++ tests/testthat/test-get_search_params.R
@@ -1,20 +1,10 @@
 
-invs <- get_search_params("investigators")
-spp <- get_search_params("species")
-
-
-test_that("Investigator search results are a character string", {
-  expect_type(invs, "character")
+test_that("Investigator search", {
+  expect_type(get_search_params("investigator"), "character")
+  expect_gte(length(get_search_params("investigator")), 251)
 })
 
-test_that("There are many investigators in the search", {
-  expect_gte(length(invs), 251)
-})
-
-test_that("Species returns a data frame", {
-  expect_s3_class(spp, "data.frame")
-})
-
-test_that("There are many species in the search", {
-  expect_gte(nrow(spp), 57)
+test_that("Species search", {
+  expect_s3_class(get_search_params("species"), "data.frame")
+  expect_gte(nrow(get_search_params("species")), 57)
 })
