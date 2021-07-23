f92c13cba6325728c1caf32e88e29ad241a3cf33
Chris Guiterman <chguiterman@email.arizona.edu>
2021-07-23 10:56:29 -0400

    test search_impd()

---
+++ tests/testthat/test-search_impd.R
@@ -0,0 +1,19 @@
+
+library(stringr)
+
+quga_search <- search_impd(species = "QUGA")
+swetnam_search <- search_impd(investigators = "Swetnam")
+
+test_that("Returns a data frame", {
+  expect_s3_class(quga_search, "data.frame")
+})
+
+test_that("Species are correct", {
+  quga_in_search <- str_extract_all(quga_search$species, "QUGA")
+  expect_gte(length(quga_in_search), 5)
+})
+
+test_that("Investigators are correct", {
+  tom_in_search <- str_extract_all(swetnam_search$investigators, "Swetnam")
+  expect_gte(length(tom_in_search), 78)
+})
