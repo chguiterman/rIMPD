85bf0c15da9c5c55422ad1d024aaa3ac8a9159c3
Chris Guiterman <chguiterman@email.arizona.edu>
2021-07-29 12:57:13 -0400

    Add full IMPD search result to package

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' IMPD search results
+#'
+#' Full list of North American tree-ring records in the IMPD
+#'
+#' @format A data frame with 14 variables: \code{uuid}, \code{NOAAStudyID},
+#'   \code{NOAASiteID}, \code{siteName}, \code{studyCode}, \code{investigators},
+#'   \code{first_year}, \code{last_year}, \code{doi}, \code{contr_year},
+#'   \code{Latitude},\code{Longitude}, \code{species}, and \code{reference}.
+"impd_meta"
