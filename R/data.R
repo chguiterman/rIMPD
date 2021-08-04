d2d3d909fc2a39a8fa288fd111023b911003cd3d
Chris Guiterman <chguiterman@email.arizona.edu>
2021-08-04 12:01:39 -0400

    update back-up of IMPD meta

--- R/data.R
+++ R/data.R
@@ -2,8 +2,8 @@
 #'
 #' Full list of North American tree-ring records in the IMPD
 #'
-#' @format A data frame with 14 variables: \code{uuid}, \code{NOAAStudyID},
-#'   \code{NOAASiteID}, \code{siteName}, \code{studyCode}, \code{investigators},
-#'   \code{first_year}, \code{last_year}, \code{doi}, \code{contr_year},
-#'   \code{Latitude},\code{Longitude}, \code{species}, and \code{reference}.
+#' @format A data frame with 15 variables: \code{siteName}, \code{studyCode},
+#' \code{investigators}, \code{first_year}, \code{last_year}, \code{latitude},\code{longitude},
+#' \code{elevation}, \code{species}, \code{reference}, \code{doi}, \code{contr_year},
+#' \code{NOAAStudyID}, \code{NOAASiteID}, and \code{url}.
 "impd_meta"
