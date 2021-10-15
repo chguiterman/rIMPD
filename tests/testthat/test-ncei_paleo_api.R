8fed8f80e287e077928637f6ad5eef602ad8fd1c
Chris Guiterman <chguiterman@email.arizona.edu>
2021-10-15 15:26:49 -0400

    change test of url to see if it works online. This will help future changes to the search url from triggering errors when they might actually work anyway.

--- tests/testthat/test-ncei_paleo_api.R
+++ tests/testthat/test-ncei_paleo_api.R
@@ -1,9 +1,8 @@
 
 swetnam_search <- ncei_paleo_api(investigators = "Swetnam")
 
-test_that("URL is properly built", {
-  expect_equal(swetnam_search$url,
-               "https://www.ncdc.noaa.gov/paleo-search/study/search.json?metadataOnly=true&dataPublisher=NOAA&dataTypeId=12&locations=Continent%3ENorth%20America&searchText=.fhx&timeMethod=entireOver&timeFormat=CE&investigators=Swetnam")
+test_that("URL is properly built and functions online", {
+  expect_false(httr::http_error(swetnam_search[["url"]]))
 })
 
 test_that("Error message works for bad search", {
