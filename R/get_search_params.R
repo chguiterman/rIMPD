c34eb7609c1964d43f7f02a6b07e0a39323ddfa9
Chris Guiterman <chguiterman@email.arizona.edu>
2021-10-15 14:22:07 -0400

    add location (country, state/province) to search params

--- R/get_search_params.R
+++ R/get_search_params.R
@@ -1,21 +1,23 @@
 #' Extract IMPD search parameters for `investigators` and `species`
 #'
-#' This function pulls from the updated store of contributor names and species
-#' to help users specify their searches.
+#' Retrieve the updated store of contributor names, site locations, and tree
+#' species to help specify IMPD searches.
 #'
-#' @param output Two data sets are available, "investigators" (the default) and
-#'   "species"
+#' @param output Three data sets are available: "investigators" (the default),
+#'   "location", and "species"
 #'
 #' @note The list of investigators names also includes those who have
 #'   contributed to the charcoal and pollen databases of the IMPD. Some names
 #'   may not generate search results here because `rIMPD` specifies tree-ring
-#'   records.
+#'   records. Also, apologies to Canadians for using "state" as a
+#'   variable name in the output for "location"
 #'
 #' @importFrom httr GET content
 #' @importFrom jsonlite fromJSON
-#' @importFrom stringr str_split
+#' @importFrom stringr str_split str_detect
 #' @importFrom tibble as_tibble
 #' @importFrom stats setNames
+#' @importFrom dplyr filter
 #'
 #' @export
 #'
@@ -26,22 +28,39 @@
 #' # Retrieve a data frame of species codes
 #' get_search_params("species")
 
-get_search_params <- function(output = c("investigators", "species")) {
+get_search_params <- function(output = c("investigators", "location", "species")) {
   resp <- GET("https://www.ncdc.noaa.gov/paleo-search/study/params.json")
 
   params <- fromJSON(content(resp, "text", encoding = "UTF-8"),
                      simplifyDataFrame = TRUE)
 
   investigators <- params[["investigators"]][["NOAA"]][["12"]]
+
   species_list <- str_split(params[["species"]][["NOAA"]][["12"]], ":")
   species_df <- do.call(rbind, species_list) %>%
     as_tibble(.name_repair = "minimal") %>%
     setNames(c("species", "spp_code"))
 
+  location_list <- params[["locations"]][["NOAA"]][["12"]]
+  NA_locs_list <- location_list[
+    str_detect(location_list, "Continent>North America>")
+  ] %>%
+    str_split(">")
+  loc_df <- do.call(rbind,
+                    NA_locs_list[lengths(NA_locs_list) > 3]
+                    ) %>%
+    as_tibble(.name_repair = "minimal") %>%
+    setNames(c("setting", "continent", "country", "state")) %>%
+    select(3:4) %>%
+    filter(.data$country != "Central America")
+
   output <- match.arg(output)
 
   if (output == "investigators") {
     out <- investigators
+  }
+  if (output == "location") {
+    out <- loc_df
   }
   if (output == "species") {
     out <- species_df
